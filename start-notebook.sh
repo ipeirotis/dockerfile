95a1f721ee58a081a0d50262401397cff77c5d0b
Panos Ipeirotis <ipeirotis@gmail.com>
2020-06-11 16:02:18 -0400

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -6,14 +6,6 @@ set -e
 # Overwrite permission changes when mounting persistent volumes.
 sudo chown ubuntu /home/ubuntu
 sudo chgrp users /home/ubuntu
-# sudo chmod -R 755 /home/ubuntu
-
-# Assumes that a .netrc has been created in the volume
-# TODO: upload an encrypted file, decrypt before the build,
-# and copy to Dockerfile. Probably need to make the registry
-# private as well
-echo "$NETRC" > $HOME/.netrc
-chmod 600 $HOME/.netrc
 
 git config --global user.name "Panos Ipeirotis"
 git config --global user.email "ipeirotis@gmail.com"
