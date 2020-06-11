63f78f6422257eabf2c8ef730ca50f0d43bcdcf8
Panos Ipeirotis <ipeirotis@gmail.com>
2020-06-11 10:37:32 -0400

    Create start-notebook.sh

---
+++ start-notebook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# If any command fails, exit with the code of the failing command
+set -e
+
+# Overwrite permission changes when mounting persistent volumes.
+sudo chmod -R 777 /home/ubuntu
+sudo chown ubuntu /home/ubuntu
+
+exec jupyter notebook --no-browser --ip=0.0.0.0 --port=8888
